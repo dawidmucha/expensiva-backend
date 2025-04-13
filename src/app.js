85dbfefa6e22054bb7f974d0724a6357bf412495
Dawid Mucha <dawidmucha98@gmail.com>
2025-04-13 20:34:42 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+import mysql from 'mysql2/promise'
+
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'expensiva'
+})
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', async (req, res) => {
+  let results, fields
+
+  try {
+    [results, fields] = await connection.query('select * from user')
+
+    console.log(results)
+  } catch (err) {
+    console.log(err)
+  }
+
+  res.send({
+    message: 'hello world',
+    results
+  })
+})
+
+app.listen(process.env.PORT || 8081)
